0a2923b9ef17dd8059c1957eaa52dff79847b1ea
simpx <simpxx@gmail.com>
2011-06-12 20:44:01 +0800

    edit IotaException in define

--- error/IotaException.php
+++ error/IotaException.php
@@ -6,10 +6,10 @@
  * @define IE_USER_ERROR 100
  * @define IE_DB_ERROR 1000
  */
-define(IE_IOTA_ERROR,1);
-define(IE_APP_ERROR,10);
-define(IE_USER_ERROR,100);
-define(IE_DB_ERROR,1000);
+define('IE_IOTA_ERROR',1);
+define('IE_APP_ERROR',10);
+define('IE_USER_ERROR',100);
+define('IE_DB_ERROR',1000);
 class IotaException extends Exception {
 	protected $extra;
 	protected $level;
